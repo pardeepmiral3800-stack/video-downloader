b1f13c61c6cc4d2df96e3fb110433e7ae1d76388
deepm <deepm@example.com>
2025-11-08 16:01:38 +0530

    Update server.js and package.json

--- server/server.js
+++ server/server.js
@@ -253,6 +253,6 @@ app.use("*", (req, res, next) => {
   }
 });
 
-app.listen(PORT, () =>
-  console.log(`✅ Server running at http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () =>
+  console.log(`✅ Server running at http://0.0.0.0:${PORT}`)
 );
